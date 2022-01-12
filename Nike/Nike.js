6840e72cc75f70f041ee96c334d6d304f40e2281
Imran <89724435+Huseynlimran@users.noreply.github.com>
2022-01-12 14:56:45 +0400

    Update Nike.js

--- Nike/Nike.js
+++ Nike/Nike.js
@@ -7,20 +7,12 @@ burger.addEventListener("click", () => {
 })
 
 
-const tl = gsap.timeline({default: {ease: 'power1.out'}})
 
-tl.to(".text", { y: "0%",duration: 1, stagger: 0.25})
-
-tl.to('.slider', {y : "-100%", duration : 1.5, delay:0.5})
-tl.to('.intro', {y : "-100%", duration: 1}, "-=1")
-tl.fromTo('nav',{opacity :0 }, {opacity:1, duration: 1})
-tl.fromTo('.rt',{opacity :0 }, {opacity:1, duration: 1})
-tl.fromTo('.anim',{opacity :0 ,y:"-100%"}, {opacity:1, duration: 1,interval:0.7, y:"0%"})
 
 
 
 ScrollReveal().reveal('.top',{
-  origin: 'top' , duration: 1500, distance: '60px', interval:100, opacity : 0
+  origin: 'top' , duration: 1000, distance: '60px', interval:100, opacity : 0
 })
 ScrollReveal().reveal('.bottom',{
   origin: 'bottom' , duration: 1800, distance: '60px',opacity : 0
